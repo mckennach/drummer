3c122e65315a9e17588b98b0969075bcc27f6114
Chris Mckenna <mckennach@gmail.com>
2018-05-10 14:58:33 -0700

    Drums function.. shitty design tho. Also want to add different drum options

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DrumPads from './components/DrumPads';
+import { DrumData } from './data/DrumData';
+
 
 class App extends Component {
+
+  handleKeyPress = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    key.parentNode.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  removeKeyTransition = (e) => {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  componentWillMount(){
+    document.addEventListener('keydown', this.handleKeyPress.bind(this));
+    document.addEventListener('transitionend', this.removeKeyTransition.bind(this));
+
+  }
+
+
+componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress.bind(this));
+
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="app">
+        <DrumPads handleKeyPress={this.handleKeyPress} drumData={DrumData} />
       </div>
     );
   }
